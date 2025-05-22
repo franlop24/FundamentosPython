ac38480aa9f650da34e208da5b56c0014d0af2b6
Franlop24 <franlopbri@gmail.com>
2025-05-22 10:08:39 -0600

    Ejercicio 1 de Condicionales

---
+++ condicionales/ejercicio01.py
@@ -0,0 +1,10 @@
+################################################################################
+# Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+################################################################################
+
+numero1 = int(input('Ingresa un número: '))
+numero2 = int(input('Ingresa otro número: '))
+
+if numero1 > numero2:
+    print('El primer número es mayor')
+
