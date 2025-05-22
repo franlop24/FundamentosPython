56387359a865545021a4b4504b0385615c421614
Franlop24 <franlopbri@gmail.com>
2025-05-22 10:32:43 -0600

    Ejercicio 2 de condicionales

---
+++ condicionales/ejercicio02.py
@@ -0,0 +1,22 @@
+################################################################################
+# Algoritmo que pida un número y diga si es positivo, negativo o 0.
+################################################################################
+
+numero = int(input('Ingresa un número: '))
+
+# if numero < 0:
+#     print('El número es negativo')
+
+# if numero > 0:
+#     print('El número es positivo')
+
+# if numero == 0:
+#     print('El número es cero')
+
+if numero < 0:
+    print('El número es negativo')
+elif numero > 0:
+    print('El número es positivo')
+else:
+    print('El número es cero')
+
