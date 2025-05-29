149fa61279b411f349e63557f968383d831c98af
Franlop24 <franlopbri@gmail.com>
2025-05-29 10:33:54 -0600

    Ejercicio 2 de Ciclos

---
+++ ciclos/ejercicio02.py
@@ -0,0 +1,34 @@
+#################################################################################
+# Crea una aplicación que permita adivinar un número. La aplicación genera un 
+# número aleatorio del 1 al 100. A continuación va pidiendo números y va 
+# respondiendo si el número a adivinar es mayor o menor que el introducido,
+# a demás de los intentos que te quedan (tienes 10 intentos para acertarlo). 
+# El programa termina cuando se acierta el número (además te dice en cuantos 
+# intentos lo has acertado), si se llega al limite de intentos te muestra el 
+# número que había generado.
+#################################################################################
+
+import random
+
+aleatorio = random.randint(1, 100)
+intentos = 10
+
+while True:
+    numero = int(input('Adivina el número: '))
+    if aleatorio == numero:
+        aleatorio = intentos - 1
+        print('Adivinaste el número!')
+        print(f'Te quedaban { intentos } intentos')
+        break
+    elif numero > aleatorio:
+        print('Ingresa otro más pequeño: ')
+        intentos = intentos - 1
+    else:
+        print('Ingresa otro más grande: ')
+        intentos = intentos - 1
+
+    if intentos == 1: 
+        print('Perdiste!!')
+        break
+
+
